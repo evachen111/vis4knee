db9b5c9cfcf373dcf44fcc65d1375d7adeabc8fc
evachen111 <ssevachen@gmail.com>
2024-11-15 16:13:56 +0100

    add pop-up window of Images

--- src/Panels/Panel1/index.js
+++ src/Panels/Panel1/index.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
+import Plot from "react-plotly.js";
+import image1 from "./1.png";
+import image2 from "./2.png";
+import image3 from "./3.png";
+import image4 from "./4.png";
 
-export default class Profile extends Component {
-    render() {
-        return (
-            <div className='pane'>
-                <div className='header'>Patient Profile</div>
-                {/* <PieChart data={data} width={width} height={height} /> */}
+
+function PopupContent() {
+    return (
+        <div className="container" style={{ height: "650px", textAlign: "center" }}> 
+          <div className="row row-cols-2 h-100">
+            <div className="col" style={{ width: "50%", height: "50%", padding:"0px"}}>
+              <img src={image1} className="img-fluid w-100 h-100" />
             </div>
-        )
-    }
-}
+            <div className="col" style={{ width: "50%", height: "50%", padding:"0px" }}>
+              <img src={image2} className="img-fluid w-100 h-100"/>
+            </div>
+            <div className="col" style={{ width: "50%", height: "50%", padding:"0px" }}>
+              <img src={image3} className="img-fluid w-100 h-100" />
+            </div>
+            <div className="col" style={{ width: "50%", height: "50%", padding:"0px" }}>
+              <img src={image4} className="img-fluid w-100 h-100" />
+            </div>
+          </div>
+        </div>
+      );  
+  }
+  
+  export default PopupContent;
--- src/Panels/Panel6/image.js
+++ src/Panels/Panel6/image.js
@@ -1,15 +1,29 @@
-import React, { Component } from 'react';
+import React from "react";
 import Plot from "react-plotly.js";
-import image1 from './1.jpg';
-
+import Popup from "reactjs-popup";
+import image1 from "./1.jpg";
+import "reactjs-popup/dist/index.css";
+import PopupContent from "../Panel1/index";
 
 function Image() {
-    return (
-        <div className='pane'>
-            <div className='header'>3D Image</div>
-            <img src={image1} style={{ width: "95%", height: "auto", margin: "5px"}}></img>
-        </div>
-    )
-  }
-  
-  export default Image;
+  return (
+    <div className="pane">
+      <div className="header">3D Image</div>
+      <Popup
+        trigger={
+          <img
+            src={image1}
+            style={{ width: "95%", height: "auto", margin: "5px", cursor: "pointer" }}
+            alt="3D Visualization"
+          />
+        }
+        modal
+        closeOnDocumentClick
+      >
+        <PopupContent/>
+      </Popup>
+    </div>
+  );
+}
+
+export default Image;
